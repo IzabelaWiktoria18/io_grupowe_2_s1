3573cb84d5099b1f7dfd10c185644e62a4b9696b
Piotr Kowalczyk <56620@student.ue.poznan.pl>
2024-06-02 17:00:43 +0200

    dodano plik z samym zadaniem bo jestem leniwy i nie chce mi się teraz poprawiać całego maina grupie żeby mi działało (mergowałem ostatni)

---
+++ zad3.py
@@ -0,0 +1,17 @@
+def licz_sume(adict):
+    galeon_coins = adict["galeon"]
+    sykl_coins = adict["sykl"]
+    knut_coins = adict["knut"]
+    all_galeons = sum(galeon_coins)
+    all_sykls = sum(sykl_coins)
+    all_knuts = sum(knut_coins)
+    if all_knuts > 20:
+        to_add = int(all_knuts/21)
+        all_sykls += to_add
+        all_knuts -= to_add * 21
+    if all_sykls > 16:
+        to_add = int(all_sykls/17)
+        all_galeons += to_add
+        all_sykls -= to_add * 17
+    new_dict = {"galeon": all_galeons, "sykl" : all_sykls, "knut": all_knuts}
+    return new_dict
