ae08508b8c14f7b30358ecb5db6f5c7ffa183e17
Jakub Ginczelewski <83779@student.ue.poznan.pl>
2024-06-05 18:16:35 +0200

    Zmieniono plik z testami

--- tests_4JG.py
+++ tests_4JG.py
@@ -1,11 +1,8 @@
 import main;
 
-def wybierz_sowe_zwroc_koszt():
-    assert main.wybierz_sowe_zwroc_koszt(True, 'lokalna', 'list', 'wyjec') == wynik, "niepoprawny wynik"
-
-
-test_wybierz_sowe_zwroc_koszt(False, main.Odleglosc.LOKALNA, main.TypPaczki.LIST, main.SpecjalnaPaczka.WYJEC, {"galeon": 0, "sykl": 0, "knut": 6})
-test_wybierz_sowe_zwroc_koszt(True, main.Odleglosc.DALEKOBIEZNA, main.TypPaczki.PACZKA, main.SpecjalnaPaczka.NIE_DOTYCZY, {"galeon": 0, "sykl": 2, "knut": 8})
+def test_wybierz_sowe_zwroc_koszt():
+        assert(wybierz_sowe_zwroc_koszt(False, 'lokalna', 'list', 'wyjec'), {"galeon" : 0,"sykl" : 0, "knut" : 13})
+        assert(wybierz_sowe_zwroc_koszt(True, 'krajowa', 'list', 'list gończy'), {'galeon': 0, 'sykl': 1, 'knut': 19})
 
 
 
