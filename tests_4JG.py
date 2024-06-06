6b34ebd1352123a8cb473c53b542c943c5ca883b
Jakub Ginczelewski <83779@student.ue.poznan.pl>
2024-06-06 17:03:15 +0200

    Poprawiono plik z testami

--- tests_4JG.py
+++ tests_4JG.py
@@ -1,8 +1,8 @@
 import main;
 
-def test_wybierz_sowe_zwroc_koszt():
-        assert(wybierz_sowe_zwroc_koszt(False, 'lokalna', 'list', 'wyjec'), {"galeon" : 0,"sykl" : 0, "knut" : 13})
-        assert(wybierz_sowe_zwroc_koszt(True, 'krajowa', 'list', 'list gończy'), {'galeon': 0, 'sykl': 1, 'knut': 19})
+def test_wybierz_sowe_zwroc_koszt(self):
+        self.assertEqual(wybierz_sowe_zwroc_koszt(False, 'lokalna', 'list', 'wyjec'), {"galeon" : 0,"sykl" : 0, "knut" : 13})
+        self.assertEqual(wybierz_sowe_zwroc_koszt(True, 'krajowa', 'list', 'list gończy'), {'galeon': 0, 'sykl': 1, 'knut': 19})
 
 
 
